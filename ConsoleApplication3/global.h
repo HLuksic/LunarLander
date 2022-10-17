f252510d6ffda6fcb43fcdac685dd79a959a90c2
Hrvoje Lukšić <hluksic20@foi.hr>
2022-10-17 19:30:23 +0200

    Fixed global variables

--- ConsoleApplication3/global.h
+++ ConsoleApplication3/global.h
@@ -2,14 +2,14 @@
 
 #include <random>
 
-extern constexpr uint16_t SCREEN_WIDTH  = 550;
-extern constexpr uint16_t SCREEN_HEIGHT = 390;
-extern constexpr float    PI            = 3.141592f;
-extern constexpr float    HALF_PI       = PI / 2;
-extern constexpr float    PLAYER_X      = SCREEN_WIDTH / 2;
-extern constexpr float    PLAYER_Y      = SCREEN_HEIGHT / 2;
-extern inline float       Scale         = 0.5f;
-extern inline bool        Paused        = false;
+extern inline constexpr uint16_t SCREEN_WIDTH  = 550;
+extern inline constexpr uint16_t SCREEN_HEIGHT = 390;
+extern inline constexpr float    PI            = 3.141592f;
+extern inline constexpr float    HALF_PI       = PI / 2;
+extern inline constexpr float    PLAYER_X      = SCREEN_WIDTH / 2;
+extern inline constexpr float    PLAYER_Y      = SCREEN_HEIGHT / 2;
+extern inline float				 Scale         = 0.5f;
+extern inline bool				 Paused        = false;
 
 inline float RandInRange(float a, float b) { return ((b - a) * ((float)rand() / RAND_MAX)) + a; }
 
